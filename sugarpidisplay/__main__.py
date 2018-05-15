a46b40c3434fd4c66ea67e8ce4f4519498d497c7
bassettb <>
2018-05-15 00:52:55 -0400

    Fix the html for the config page

--- sugarpidisplay/__main__.py
+++ sugarpidisplay/__main__.py
@@ -1,4 +1,7 @@
 
+if __name__ == '__main__' and __package__ is None:
+    from os import sys, path
+    sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
 if __name__ == "__main__":
 	from sugarpidisplay.sugar_pi_app import SugarPiApp
 	app = SugarPiApp()
